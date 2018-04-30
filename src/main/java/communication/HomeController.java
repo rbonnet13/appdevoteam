15318cbee3d0285b0b13452c0063416768a83680
rbonnet <rbonnet@FRL0437.fr.devoteam.com>
2018-04-30 16:57:17 +0200

    up

--- src/main/java/communication/HomeController.java
+++ src/main/java/communication/HomeController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Value;
+
 @Controller
 public class HomeController {
 	// inject via application.properties
