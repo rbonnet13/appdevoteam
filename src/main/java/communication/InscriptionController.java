0038e6d1a0fcd9f0c6abb0d5ba92bd192898364e
rbonnet <rbonnet@FRL0437.fr.devoteam.com>
2018-04-30 16:16:59 +0200

    up

--- src/main/java/communication/InscriptionController.java
+++ src/main/java/communication/InscriptionController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ConnexionController {
+public class InscriptionController {
 
     @RequestMapping("/inscription.html")
     public String sayHello(){
