3eeefd3bebe2f0ceaec7899713ffb02439a07f87
rbonnet <rbonnet@FRL0437.fr.devoteam.com>
2018-04-30 17:02:51 +0200

    up

--- src/main/java/communication/ConnexionController.java
+++ src/main/java/communication/ConnexionController.java
@@ -1,17 +1,13 @@
 package appdevoteam;
 import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
-import org.springframework.beans.factory.annotation.Value;
-@Controller
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ConnexionController {
-	// inject via application.properties
-		@Value("${welcome.message:test}")
-		private String message = "Hello World";
-		
+
     @RequestMapping("/connexion.html")
-    public String welcome(Map<String, Object> model) {
-		model.put("message", this.message);
-		return "welcome";
-	}
+    public String sayHello(){
+        return ("Hello, SpringBoot on Wildfly");
+    }
 } 
--- src/main/java/communication/HomeController.java
+++ src/main/java/communication/HomeController.java
@@ -1,19 +1,15 @@
 package appdevoteam;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
-import org.springframework.beans.factory.annotation.Value;
 
-@Controller
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
-	// inject via application.properties
-		@Value("${welcome.message:test}")
-		private String message = "Hello World";
 		
     @RequestMapping("/index.html")
-    public String welcome(Map<String, Object> model) {
-		model.put("message", this.message);
-		return "welcome";
-	}
+    public String sayHello(){
+        return ("Hello, SpringBoot on Wildfly");
+    }
 } 
