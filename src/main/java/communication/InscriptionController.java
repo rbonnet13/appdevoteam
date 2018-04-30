2eb1018a5dfdc75fa269d23acdb409b4339992c2
rbonnet <rbonnet@FRL0437.fr.devoteam.com>
2018-04-30 16:11:47 +0200

    update

---
+++ src/main/java/communication/InscriptionController.java
@@ -0,0 +1,13 @@
+package appdevoteam;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConnexionController {
+
+    @RequestMapping("/inscription.html")
+    public String sayHello(){
+        return ("Hello, SpringBoot on Wildfly");
+    }
+}
