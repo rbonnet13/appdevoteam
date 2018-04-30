d1b6dcd9b56b99d9184adb7c1ad66ffae16db343
rbonnet <rbonnet@FRL0437.fr.devoteam.com>
2018-04-30 12:32:15 +0200

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-package org.exemple.demo;
 
 /**
  * Hello world!
