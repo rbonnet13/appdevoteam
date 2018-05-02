3626bcfc11c412c2601d0af56ede8898969a05b3
rbonnet <rbonnet@192.168.56.1>
2018-05-02 14:55:28 +0200

    update

--- src/main/java/appdevoteam/repository/UserRepository.java
+++ src/main/java/appdevoteam/repository/UserRepository.java
@@ -2,7 +2,7 @@ package appdevoteam.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import hello.User;
+import appdevoteam.entity.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
