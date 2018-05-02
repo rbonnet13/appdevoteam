5b333ce59b62efd1289135265f6dda01a6d233de
rbonnet <rbonnet@192.168.56.1>
2018-05-02 15:19:48 +0200

    up

--- src/main/java/appdevoteam/communication/HomeController.java
+++ src/main/java/appdevoteam/communication/HomeController.java
@@ -4,10 +4,9 @@ import java.util.Map;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-
-@RestController
+@Controller
 public class HomeController {
 			
 	 @RequestMapping("/")
