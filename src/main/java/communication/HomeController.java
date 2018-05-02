b34d30bd7287f43708f4f3bc2a068403488cd323
rbonnet <rbonnet@192.168.56.1>
2018-05-02 10:00:46 +0200

    up

--- src/main/java/communication/HomeController.java
+++ src/main/java/communication/HomeController.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-		
+			
     @RequestMapping("/")
-    public String welcome(Map<String, Object> model) {
-		model.put("DEVOTEAM", this.message);
+    public String welcome() {
 		return "index";
 	}
-} 
+}
