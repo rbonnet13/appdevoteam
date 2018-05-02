1ba1ae4b89c1e74c2beb383152169352ffd73c3f
rbonnet <rbonnet@192.168.56.1>
2018-05-02 16:43:16 +0200

    up

--- src/main/java/communication/UserController.java
+++ src/main/java/communication/UserController.java
@@ -12,9 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class UserController {
 
-	@RequestMapping("/inscription")
-	public String newUser(Model model) {
-		model.addAttribute("user", new User());
+	@RequestMapping("/")
+	public String newUser() {
 		return "inscription";
 	}
 }
