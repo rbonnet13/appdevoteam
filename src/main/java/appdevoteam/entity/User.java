c45c90a90270f3a814333a69223c211f82e74029
rbonnet <rbonnet@192.168.56.1>
2018-05-02 15:02:20 +0200

    up

--- src/main/java/appdevoteam/entity/User.java
+++ src/main/java/appdevoteam/entity/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity // This tells Hibernate to make a table out of this class
 @Table(name = "user_table")
