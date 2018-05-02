ddc11e245b8fea0a3b2885c822a31128a8ba3d41
rbonnet <rbonnet@192.168.56.1>
2018-05-02 11:46:38 +0200

    up

--- src/main/java/appdevoteam/communication/HomeController.java
+++ src/main/java/appdevoteam/communication/HomeController.java
@@ -10,10 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 			
-	 @RequestMapping("/index")
-	    public String sayHello(){
-	        return ("Hello, SpringBoot on Wildfly");
-	    }
 	 @RequestMapping("/")
 	    public String Hello(){
 	        return ("Hello, SpringBoot on Wildfly");
